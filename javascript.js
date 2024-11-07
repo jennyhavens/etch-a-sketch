876ae7742629cb3157bb45e747a7db18c05e1340
jennyhavens <jennifer.havens91@gmail.com>
2024-11-07 17:49:55 -0600

    Add ability to hover and draw in random colors

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
 const gridContainer = document.querySelector('.grid-container');
+const changeButton = document.querySelector('#change-grid');
+const randomColorButton = document.querySelector('#colors');
+const blackWhiteButton = document.querySelector('#black-white');
+const whiteBlackButton = document.querySelector('#white-black');
+const opacityButton = document.querySelector('#opacity');
+const resetButton = document.querySelector('#reset-grid');
+//const for buttons here
 
 let gridSize = 16;
 
@@ -9,28 +16,28 @@ function createGrid(gridSize) {
             gridSquare.style.width = `${40 / gridSize}vw`;
             gridSquare.style.height = `${40 / gridSize}vw`;
             gridContainer.appendChild(gridSquare);
-        };
 
-    const allSquares = document.querySelectorAll('.square');
-    allSquares.forEach(square => {
-        let r = (Math.random() * 255);
-        let g = (Math.random() * 255);
-        let b = (Math.random() * 255);
-        let randomRGB = `rgb(${r}, ${g}. ${b})`;
-
-        square.addEventListener("mouseover", () => {
-            if(square.style.backgroundColor === "") {
-                square.style.backgroundColor = randomRGB;
-            } else {
-                square.style.opacity -= "-0.1";
-            }
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.backgroundColor = getRandomColor();
         });
-    });    
+    };
 };
+createGrid(gridSize);
 
-document.addEventListener("DOMContentLoaded", () => {
-    createGrid(gridSize);
-});
+//function drawingOnGrid() {
+   // const squares = document.querySelectorAll('.square');
+    //squares.forEach((square) => {
+        //square.addEventListener("mouseover", () => {
+            //if(isLeftClick) {
+                //if(isRainbow) {
+                    //item.style.backgroundColor = "hsl(" + Math.floor(Math.random() * (355 -1) + 1) + ",65%,65%)";
+                //} else {
+                   //item.style.backgroundColor = selectedColor;
+                //}
+        //squares.addEventListener('mouseover', () => {
+            //squares.style.backgroundColor = getRandomColor();
+       // });
+//}
 
 function resetGrid() {
     while (gridContainer.firstChild) {
@@ -38,26 +45,38 @@ function resetGrid() {
     };
 };
 
-const changeButton = document.querySelector('#change-grid');
 changeButton.addEventListener('click', () => {
     const userInput = window.prompt("Choose new grid dimensions (enter 1-100):");
-
     if (userInput > 0 && userInput <= 100) {
         gridSize = userInput;
         resetGrid();
         createGrid(gridSize);
     } else if (isNaN(userInput)) {
-        alert("Please input a number");
+        alert("Please input a number between 1 and 100");
     } else {
         alert("The number must be between 1 and 100");
     };  
 });
 
-const resetButton = document.querySelector('#reset-grid');
+//const allSquares = document.querySelectorAll('.square');
+//allSquares.forEach(square => {
+    //if(square.style.backgroundColor === "") {
+        //square.style.backgroundColor = getRandomColor;
+    //}
+//});
+
+function getRandomColor() {
+    var R = Math.floor(Math.random() * 256);
+    var G = Math.floor(Math.random() * 256);
+    var B = Math.floor(Math.random() * 256);
+    return `rgb(${R}, ${G}, ${B})`;
+};
+
+randomColorButton.addEventListener('click', () => {
+    getRandomColor();
+});
+
 resetButton.addEventListener('click', () => {
-    const allSquares = document.querySelectorAll(".square");
-    allSquares.forEach((square) => {
-        square.style.backgroundColor = "";
-        square.style.opacity = "";
-    });
-}); 
+    resetGrid();
+    createGrid(gridSize);
+});
