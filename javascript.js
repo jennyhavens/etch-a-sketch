4b63338e4246b4d67404af519ea2bac694e33cf9
jennyhavens <jennifer.havens91@gmail.com>
2024-11-06 18:01:31 -0600

    Add functionality to change grid button

--- javascript.js
+++ javascript.js
@@ -1,22 +1,63 @@
-const grid = document.querySelector('.gridContainer');
+const gridContainer = document.querySelector('.grid-container');
 
-    let gridSize = 16;
+let gridSize = 16;
 
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize**2; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('square');
+            gridSquare.style.width = `${40 / gridSize}vw`;
+            gridSquare.style.height = `${40 / gridSize}vw`;
+            gridContainer.appendChild(gridSquare);
+        };
 
+    const allSquares = document.querySelectorAll('.square');
+    allSquares.forEach((square) => {
+        let r = (Math.random() * 255);
+        let g = (Math.random() * 255);
+        let b = (Math.random() * 255);
+        let randomRGB = `rgb(${r}, ${g}. ${b})`;
 
-    function createGrid(gridSize) {
-        for (i = 0; i < gridSize * gridSize; i++) {
-            const oneSquare = document.createElement('div');
-            oneSquare.classList.add('square-style');
-            oneSquare.dataset.opacityLevel = '0';
-            oneSquare.dataset.inreasing = true;
-            grid.appendChild(oneSquare);
-        }
-    }
+        square.addEventListener('mouseover', () => {
+            if(square.style.backgroundColor === "") {
+                square.style.backgroundColor = randomRGB;
+            } else {
+                square.style.opacity -= "-0.1";
+            }
+        });
+    });    
+};
 
-    const squareSize = 100 / gridSize;
-    const squares = document.querySelector('.square-style');
-
+document.addEventListener("DOMContentLoaded", () => {
     createGrid(gridSize);
+});
 
+function resetGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    };
+};
 
+const changeButton = document.querySelector("#change-grid");
+changeButton.addEventListener('click', () => {
+    const userInput = window.prompt("Choose new grid dimensions (enter 1-100):");
+
+    if (userInput > 0 && userInput <= 100) {
+        gridSize = userInput;
+        resetGrid();
+        createGrid(gridSize);
+    } else if (isNaN(userInput)) {
+        alert("Please input a number");
+    } else {
+        alert("The number must be between 1 and 100");
+    };  
+});
+
+const resetButton = document.querySelector('#reset-grid');
+resetButton.addEventListener('click', () => {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square) => {
+        square.style.backgroundColor = "";
+        square.style.opacity = "";
+    });
+}); 
